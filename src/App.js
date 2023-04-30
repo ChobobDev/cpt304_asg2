e45a45856ecec1ea1ca4ec232614bb0cf60a04bf
chobobdev <chobobdev@naver.com>
2023-05-01 00:49:13 +0900

    feat: render components implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import logo from './logo.svg';
+import CountrySelector from './components/CountrySelector';
+import PublicHolidayList from './components/PublicHolidayList';
+import Location from './components/Location';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const  App=()=> {
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [publicHoliday, setPublicHoliday] = useState(null);
+  const handleCountryChange = (countryCode) => {
+    setSelectedCountry(countryCode);
+    console.log(countryCode);
+  };
+  const handlePublicHoliday = (holiday) => {
+    setPublicHoliday(holiday);
+  };
   return (
     <div className="App">
+      <Location/>
+      <CountrySelector onCountryChange={handleCountryChange} />
+      <PublicHolidayList countryCode={selectedCountry} onHolidaySelect={handlePublicHoliday} />
     </div>
   );
 }
